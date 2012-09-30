25236cb42113750e6d60c1a8b6f8c2247bd46f80
Carlos Iriarte <ciriarte@gmail.com>
2012-09-30 15:07:23 -0700

    Cleaning up a bit

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ var express = require('express')
 
 i18n.configure({
   // setup some locales - other locales default to en silently
-  locales:['en', 'es', 'de'],
-
-  // where to register __() and __n() to, might be "global" if you know what you are doing
-  register: global
+  locales:['en', 'es', 'de']
 });
 
 var app = express();
@@ -21,13 +18,18 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'hjs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(i18n.init);
-  app.use(app.router);
+  app.use(function(req, res, next) {
+    res.locals.__i = i18n.__;
+    res.locals.__n = i18n.__n;
+    next();
+  });
+ app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -42,7 +44,7 @@ app.all('/*', function(req, res, next) {
   } else {
     next();     
   }
-})
+});
 
 app.get('/', routes.index);
 
--- routes/index.js
+++ routes/index.js
@@ -4,44 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('resume', { 
-  	fullname: __('Carlos Iriarte'),
-    description: __('Technical Architect &amp; Technology Dissident'),
-  	current_position: __('Lead Architect'),
-    telephone: __('+1 (310) 990-7882'),
-    streetAddress: __('409 S Fuller Ave.'),
-    addressLocality: __('Los Angeles'),
-    addressRegion: __('California'),
-    addressCountry: __('U.S.A.'),
-    postalCode: __('90036'),
-    follow_twitter: __('Follow me on Twitter'),
-    about_me_title: __('about_me_title'),
-    summary: __('summary'),
-    specialties_title: __('specialties_title'),
-    specialties: __('specialties'),
-    skills: __('Skills'),
-    experience_title: __('experience_title'),
-    education_title: __('education_title'),
-    february: __('february'),
-    september: __('September'),
-    october: __('October'),
-    august: __('August'),
-    present: __('Present'),
-    march: __('March'),
-    november: __('November'),
-    december: __('December'),
-    itesm_job_description: __('itesm_job_description'),
-    dial800_job_description: __('dial800_job_description'),
-    neoris_job_description: __('neoris_job_description'),
-    tesis_job_description: __('tesis_job_description'),
-    volkswagen_job_description: __('volkswagen_job_description'),
-    major: __('Mechatronics Engineering, B.Sc.'),
-    csharp_tooltip: __('csharp_tooltip'),
-    javascript_tooltip: __('javascript_tooltip'),
-    python_tooltip: __('python_tooltip'),
-    vim_tooltip: __('vim_tooltip'),
-    git_tooltip: __('git_tooltip'),
-    vs_studio_tooltip: __('vs_studio_tooltip'),
-    nodejs_tooltip: __('nodejs_tooltip')
-  });
+  res.render('resume');
 };
